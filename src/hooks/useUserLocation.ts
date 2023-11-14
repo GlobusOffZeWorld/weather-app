dbd6ce9a61b4342201b46ce083ca0bc93c73fe1b
GlobusOffZeWorld <gleb737.gl@gmail.com>
2023-11-15 01:34:57 +0300

    fix: dumping city when reload page

--- src/hooks/useUserLocation.ts
+++ src/hooks/useUserLocation.ts
@@ -1,23 +1,27 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { setUserLocation } from '@/redux/slices/userLocationSlice';
+import { RootState } from '@/redux/store';
 
 export const useCurrentUserLocation = () => {
+  const userLocation = useSelector((state: RootState) => state.userLocation);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getUserLocation = async () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          position => {
-            const { latitude, longitude } = position.coords;
-            dispatch(setUserLocation({ cityName: 'Current position', latitude, longitude }));
-          },
-          error => console.error('Error getting user location', error)
-        );
-      } else {
-        console.error('Geolocation is not supported by this browser.');
+      if (userLocation.cityName === '') {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            position => {
+              const { latitude, longitude } = position.coords;
+              dispatch(setUserLocation({ cityName: 'Current position', latitude, longitude }));
+            },
+            error => console.error('Error getting user location', error)
+          );
+        } else {
+          console.error('Geolocation is not supported by this browser.');
+        }
       }
     };
     getUserLocation();
